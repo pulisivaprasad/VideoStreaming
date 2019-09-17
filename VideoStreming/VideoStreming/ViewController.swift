f354b2047e66ad61440b2168544d0ceecba9bc3b
pulisivaprasad <reddy.siva462@gmail.com>
2019-09-17 12:29:04 +0530

    Pod install

---
+++ VideoStreming/VideoStreming/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  VideoStreming
+//
+//  Created by sivaprasad reddy on 17/09/19.
+//  Copyright © 2019 Wipro. All rights reserved.
+//
+
+import UIKit
+import SwiftVideoGenerator
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
